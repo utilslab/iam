9a3b832846b90b02b9ba9c6c244f4da7a8fd3eef
koyeo <koeyo@qq.com>
2022-04-08 02:50:34 +0800

    add axios

--- exporter/axios.go
+++ exporter/axios.go
@@ -18,10 +18,31 @@ func (a AxiosMaker) Make(pkg string, methods []*Method) (files []*File, err erro
 	}
 	serviceFile := new(File)
 	serviceFile.Name = "service.make.ts"
-	serviceFile.Content, err = Render(angularServiceTpl, data, EmptyFormatter)
+	serviceFile.Content, err = Render(axiosServiceTpl, data, EmptyFormatter)
 	if err != nil {
 		return
 	}
 	files = append(files, serviceFile)
 	return
 }
+
+const axiosServiceTpl = `import axios, {AxiosPromise, AxiosRequestConfig} from 'axios';
+{% for method in Methods %}
+{% if method.Description %}// {{ method.Description }}{% endif %}
+export function {{ method.Name }}({% if method.InputType !='' %}params: {{ method.InputType }}, {% endif %}request?: AxiosRequestConfig): {% if method.OutputType !='' %}AxiosPromise<{{ method.OutputType }}>{% else %}AxiosPromise<null>{% endif %} {
+	if (!request) {
+		request = {}
+	}
+	request.method = '{{ method.Method }}';
+	request.url = '{{ method.Path }}';
+	{% if method.InputType !='' %}{% if  method.Method == 'GET' or method.Method == 'DELETE' %}request.params = params;
+	{% else %}request.data = params;{% endif %}{% endif %}
+	return axios(request)
+}
+{% endfor %}
+{% for struct in Structs %}
+export interface {{ struct.Name }} {
+{% for field in struct.Fields %}    {{field.Param}}?: {{field.Type}}, {% if field.Label or field.Description %}// {{field.Label}} {{field.Description}}{% endif %}
+{% endfor %}}
+{% endfor %}
+`
